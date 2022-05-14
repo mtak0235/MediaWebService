2a3093b0af996d571a64b3f1ed81686aa7073dc3
mtak <jesustark0235@gmail.com>
2022-05-14 21:43:07 +0900

    #3 팝업 띄우는데에는 성공했으나 새로 생긴 팝업 윈도우로 원하는 위치에 value 못끼워넣음

--- mission1/mtak/sub3/main.js
+++ mission1/mtak/sub3/main.js
@@ -1,2 +1,21 @@
-let video_title = document.querySelector('.video-title');
-video_title.onclick = () => alert(video_title.textContent);
+function video_alert(e){
+	alert(e.target.textContent);
+}
+
+function video_popup(e){
+	let win= window.open("popup.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+	win.document.write(`<h1 id="video-title">${e.target.textContent}</h1>`);
+}
+
+function video_layer_popup(e) {
+
+}
+
+let video_titles = document.querySelectorAll('.video-title');
+for (let i = 0; i < video_titles.length; i++) {
+	// video_titles[i].addEventListener('click', video_alert);
+	// video_titles[i].addEventListener('click', video_popup);
+	video_titles[i].addEventListener('click', video_layer_popup);
+}
+
+
