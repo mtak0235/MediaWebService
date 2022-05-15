b31ccf24fa5265a683de952753ed0bed551ad988
mtak <jesustark0235@gmail.com>
2022-05-15 19:38:23 +0900

    popup

--- mission1/mtak/sub3/main.js
+++ mission1/mtak/sub3/main.js
@@ -4,8 +4,16 @@ function video_alert(e){
 
 function video_popup(e){
 	let win= window.open("popup.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
-	win.document.write(`<h1 id="video-title">${e.target.textContent}</h1>`);
+	console.log("here");
+	console.log(win.document);
+	win.onload(() => {
+		console.log(win.document);
+		let com = win.document.querySelector('.video');
+		com.innerHTML = "hi";
+	});
+	// win.document.write(`<h1 id="video-title">${e.target.textContent}</h1>`);
 }
+
 
 function video_layer_popup(e) {
 
@@ -13,8 +21,8 @@ function video_layer_popup(e) {
 
 let video_titles = document.querySelectorAll('.video-title');
 for (let i = 0; i < video_titles.length; i++) {
-	// video_titles[i].addEventListener('click', video_alert);
-	// video_titles[i].addEventListener('click', video_popup);
+	video_titles[i].addEventListener('click', video_alert);
+	video_titles[i].addEventListener('click', video_popup);
 	video_titles[i].addEventListener('click', video_layer_popup);
 }
 
