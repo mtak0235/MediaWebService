cc72af26a758cc84bbeb53d4b8421ac0763109a2
mtak0235 <jesustark0235@gmail.com>
2022-05-23 23:28:25 +0900

    #4 img를 local server로 변경. 데이터 이동으로 페이지 재구성 서버없이 어케하는지 ㅈㄴ 모르겠음

--- mission1/mtak/sub4/main.js
+++ mission1/mtak/sub4/main.js
@@ -17,8 +17,8 @@ function video_layer_popup(e) {
 
 let video_titles = document.querySelectorAll('.video-title');
 for (let i = 0; i < video_titles.length; i++) {
-	video_titles[i].addEventListener('click', video_alert);
-	video_titles[i].addEventListener('click', video_popup);
+	// video_titles[i].addEventListener('click', video_alert);
+	// video_titles[i].addEventListener('click', video_popup);
 	video_titles[i].addEventListener('click', video_layer_popup);
 }
 
