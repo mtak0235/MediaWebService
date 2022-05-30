418f5b02d326848c9479768136f45897da1732a6
mtak <jesustark0235@gmail.com>
2022-05-30 16:47:04 +0900

    #5 글자 수 제한해서 innerHTML 더보기 구현

--- mission1/mtak/sub5/detail.js
+++ mission1/mtak/sub5/detail.js
@@ -11,10 +11,12 @@ main_video_content.textContent = localStorage.getItem("main-video-content");
 
 
 document.addEventListener('DOMContentLoaded', function () { //DOM 생성 후 이벤트 리스너 등록
+    console.log("after DOMContentLoaded");
     // 더보기 버튼 이벤트 리스너
     document.querySelector('.btn_open').addEventListener('click', function (e) {
 
         let classList = document.querySelector('.main-video-content').classList; // 더보기 프레임의 클래스 정보 얻기
+
         if (classList.contains('showstep1')) {
             classList.remove('showstep1');
             document.querySelector('.btn_open').classList.add('hide');
@@ -26,7 +28,9 @@ document.addEventListener('DOMContentLoaded', function () { //DOM 생성 후 이
         }
 
         if (classList.contains('showstep2') && !classList.contains('showstep1')) {
-            document.querySelector('.main-video-content').innerHTML = localStorage.getItem("main-video-content");
+            document.querySelector('.main-video-content > .content').innerHTML = localStorage.getItem("main-video-content");
+            e.target.classList.add('hide');
+            document.querySelector('.btn_close').classList.remove('hide');
         }
 
     });
@@ -40,25 +44,25 @@ document.querySelector('.btn_close').addEventListener('click', function (e) {
 
 //컨텐츠 로딩 완료 후 높이 기준으로 클래스 재처리
 window.addEventListener('load', function () {
+    console.log('after  Loading')
     let contentHeight = document.querySelector('.main-video-content > .content').offsetHeight; //컨텐츠 높이 얻기
     if (contentHeight <= this.document.querySelector('.main-video-content > .content').style.lineHeight * 4) {
         document.querySelector('.main-video-content').classList.remove('showstep1'); // 초기값보다 작으면 전체 컨텐츠 표시
         document.querySelector('.btn_open').classList.add('hide'); // 버튼 감춤
     } else {
-        console.log("here");
-        resize_content_to_fit();
-        document.querySelector('.main-video-content').innerHTML = document.querySelector('.main-video-content').innerHTML + "...";
+        let main_video_content_short = localStorage.getItem("main-video-content").substring(0, 800) + "...";
+        document.querySelector('.main-video-content > .content').innerHTML = main_video_content_short;
     }
 });
 
-function resize_content_to_fit() {
-    let target = document.querySelector('.main-video-content');
-    target.innerHTML = target.textContent.substring(0, target.textContent.length - 1);
-    console.log(target.innerHTML);
-    console.log(document.querySelector('.main-video-content > .content'));
-    let contentHeight = document.querySelector('.main-video-content > .content').offsetHeight;
-    if (contentHeight >= document.querySelector('.main-video-content > .content').style.lineHeight * 4) {
-        resize_to_fit();
-    }
-}
+// function resize_content_to_fit() {
+//     let target = document.querySelector('.main-video-content');
+//     target.innerHTML = target.textContent.substring(0, target.textContent.length - 1);
+//     let contentHeight = document.querySelector('.main-video-content > .content').offsetHeight;
+//     if (contentHeight == document.querySelector('.main-video-content > .content').style.lineHeight * 4)
+//         return ;
+//     if (contentHeight > document.querySelector('.main-video-content > .content').style.lineHeight * 4) {
+//         resize_to_fit();
+//     }
+// }
 
