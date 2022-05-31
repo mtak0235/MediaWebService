270890bc7eeff0093ad7c293ccae1521ce049e4c
mtak <jesustark0235@gmail.com>
2022-05-31 18:04:15 +0900

    playlist 거의 완료

--- mission2/mtak/sub1/detail.js
+++ mission2/mtak/sub1/detail.js
@@ -1,58 +1,80 @@
-let main_video_stream = document.querySelector(".main-video-stream");
-let img = document.createElement("img");
-img.src = localStorage.getItem("main-video-stream");
-img.style.width = "100%";
-img.style.height = "100%";
-main_video_stream.appendChild(img);
-let main_video_title = document.querySelector(".main-video-title");
-let main_video_content = document.querySelector(".main-video-content > .content");
-main_video_title.textContent = localStorage.getItem("main-video-title");
-main_video_content.textContent = localStorage.getItem("main-video-content");
-
-
 document.addEventListener('DOMContentLoaded', function () { //DOM 생성 후 이벤트 리스너 등록
-    console.log("after DOMContentLoaded");
-    // 더보기 버튼 이벤트 리스너
-    document.querySelector('.btn_open').addEventListener('click', function (e) {
+	console.log("after DOMContentLoaded");
 
-        let classList = document.querySelector('.main-video-content').classList; // 더보기 프레임의 클래스 정보 얻기
+	let main_video_stream = document.querySelector(".main-video-stream");
+	let img = document.createElement("img");
+	img.src = localStorage.getItem("main-video-stream");
+	main_video_stream.appendChild(img);
+	let main_video_title = document.querySelector(".main-video-title");
+	let main_video_content = document.querySelector(".main-video-content > .content");
+	main_video_title.textContent = localStorage.getItem("main-video-title");
+	main_video_content.textContent = localStorage.getItem("main-video-content");
 
-        if (classList.contains('showstep1')) {
-            classList.remove('showstep1');
-            document.querySelector('.btn_open').classList.add('hide');
-        }
-        //전체보기시 더보기 버튼 감추기 & 감추기 버튼 표시
-        if (!classList.contains('showstep1') && !classList.contains('showstep2')) {
-            e.target.classList.add('hide');
-            document.querySelector('.btn_close').classList.remove('hide');
-        }
+	let playlist = document.querySelector(".playlist");
+	fetch("./data.json")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			data = data.playlist;
+			data.forEach(item => {
+				console.log(item);
+				let sub_video = document.createElement('figure');
+				sub_video.className = "sub-video";
+				let sub_img = document.createElement('img');
+				sub_img.src = item.src;
+				sub_video.appendChild(sub_img);
+				let sub_desc = document.createElement('figcaption');
+				sub_desc.textContent = item.title;
+				sub_video.appendChild(sub_desc);
+				playlist.appendChild(sub_video);
+			});
+		});
 
-        if (classList.contains('showstep2') && !classList.contains('showstep1')) {
-            document.querySelector('.main-video-content > .content').innerHTML = localStorage.getItem("main-video-content");
-            e.target.classList.add('hide');
-            document.querySelector('.btn_close').classList.remove('hide');
-        }
 
-    });
+
+		
+
+		// 더보기 버튼 이벤트 리스너
+		document.querySelector('.btn_open').addEventListener('click', function (e) {
+
+			let classList = document.querySelector('.main-video-content').classList; // 더보기 프레임의 클래스 정보 얻기
+
+			if (classList.contains('showstep1')) {
+				classList.remove('showstep1');
+				document.querySelector('.btn_open').classList.add('hide');
+			}
+			//전체보기시 더보기 버튼 감추기 & 감추기 버튼 표시
+			if (!classList.contains('showstep1') && !classList.contains('showstep2')) {
+				e.target.classList.add('hide');
+				document.querySelector('.btn_close').classList.remove('hide');
+			}
+
+			if (classList.contains('showstep2') && !classList.contains('showstep1')) {
+				document.querySelector('.main-video-content > .content').innerHTML = localStorage.getItem("main-video-content");
+				e.target.classList.add('hide');
+				document.querySelector('.btn_close').classList.remove('hide');
+			}
+
+		});
 });
 
 document.querySelector('.btn_close').addEventListener('click', function (e) {
-    e.target.classList.add('hide');
-    document.querySelector('.btn_open').classList.remove('hide');
-    document.querySelector('.main-video-content').classList.add('showstep1'); // 초기 감춤 상태로 복귀
+	e.target.classList.add('hide');
+	document.querySelector('.btn_open').classList.remove('hide');
+	document.querySelector('.main-video-content').classList.add('showstep1'); // 초기 감춤 상태로 복귀
 });
 
 //컨텐츠 로딩 완료 후 높이 기준으로 클래스 재처리
 window.addEventListener('load', function () {
-    console.log('after  Loading')
-    let contentHeight = document.querySelector('.main-video-content > .content').offsetHeight; //컨텐츠 높이 얻기
-    if (contentHeight <= this.document.querySelector('.main-video-content > .content').style.lineHeight * 4) {
-        document.querySelector('.main-video-content').classList.remove('showstep1'); // 초기값보다 작으면 전체 컨텐츠 표시
-        document.querySelector('.btn_open').classList.add('hide'); // 버튼 감춤
-    } else {
-        let main_video_content_short = localStorage.getItem("main-video-content").substring(0, 800) + "...";
-        document.querySelector('.main-video-content > .content').innerHTML = main_video_content_short;
-    }
+	console.log('after  Loading')
+	let contentHeight = document.querySelector('.main-video-content > .content').offsetHeight; //컨텐츠 높이 얻기
+	if (contentHeight <= this.document.querySelector('.main-video-content > .content').style.lineHeight * 4) {
+		document.querySelector('.main-video-content').classList.remove('showstep1'); // 초기값보다 작으면 전체 컨텐츠 표시
+		document.querySelector('.btn_open').classList.add('hide'); // 버튼 감춤
+	} else {
+		let main_video_content_short = localStorage.getItem("main-video-content").substring(0, 800) + "...";
+		document.querySelector('.main-video-content > .content').innerHTML = main_video_content_short;
+	}
 });
 
 // function resize_content_to_fit() {
@@ -65,3 +87,5 @@ window.addEventListener('load', function () {
 //         resize_to_fit();
 //     }
 // }
+
+
